f009b38a48529f46802ae78a2841d264e983c80c
Nikola Pleić <npleic20@foi.hr>
2023-11-04 12:24:50 +0100

    frontend feat: Created UI for register page

--- frontend/app/src/main/java/com/intersoft/groupup_app/navigation/RegistrationPage.kt
+++ frontend/app/src/main/java/com/intersoft/groupup_app/navigation/RegistrationPage.kt
@@ -1,8 +1,58 @@
 package com.intersoft.groupup_app.navigation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.intersoft.ui.PrimaryButton
+import com.intersoft.ui.TextInputField
+import com.intersoft.ui.TitleText
 
 @Composable
 fun RegistrationPage(onRegister: () -> Unit){
+    var email by remember {
+        mutableStateOf("")
+    }
+    var username by remember {
+        mutableStateOf("")
+    }
+    var location by remember {
+        mutableStateOf("")
+    }
+    var password by remember {
+        mutableStateOf("")
+    }
+    var passwordConfirm by remember {
+        mutableStateOf("")
+    }
 
+    Column (
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(30.dp)
+    ){
+        TitleText(text = "Register")
+        TextInputField(label = "email")
+        TextInputField(label = "username")
+        TextInputField(label = "location")
+        TextInputField(label = "password")
+        TextInputField(label = "confirm password")
+        PrimaryButton(buttonText = "Sign up", modifier = Modifier.align(Alignment.CenterHorizontally).padding(top = 40.dp)) {
+
+        }
+    }
+}
+
+@Preview
+@Composable
+fun RegistrationPagePreview(){
+    RegistrationPage{}
 }
--- frontend/core/ui/src/main/java/com/intersoft/ui/SimpleComponents.kt
+++ frontend/core/ui/src/main/java/com/intersoft/ui/SimpleComponents.kt
@@ -1,19 +1,30 @@
 package com.intersoft.ui
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
-fun PrimaryButton(buttonText: String, action: () -> Unit){
+fun PrimaryButton(buttonText: String, modifier: Modifier = Modifier, action: () -> Unit){
      Button(
          onClick = action,
+         modifier = modifier,
          colors = ButtonDefaults.buttonColors(
              containerColor = Color(R.color.primary),
              contentColor = Color(R.color.primaryText),
@@ -26,12 +37,34 @@ fun PrimaryButton(buttonText: String, action: () -> Unit){
 }
 
 @Composable
-fun TextInputField(label: String, action: (String) -> Unit){
-    Column{
+fun TextInputField(label: String, action: (String) -> Unit = {}){
+    var textValue by remember{
+        mutableStateOf("")
+    }
+    Column(modifier = Modifier.padding(20.dp)){
         Text(text = label,
-            fontSize = 8.sp,
+            fontSize = 16.sp,
             color = Color(R.color.foregroundText)
         )
-        BasicTextField("", action)
+        BasicTextField(textValue,
+            {
+                textValue = it
+                action(it)
+            },
+            singleLine = true,
+            textStyle = TextStyle(fontSize = 25.sp)
+        )
     }
+}
+
+@Composable
+fun TitleText(text: String){
+    Text(
+        text = text,
+        fontSize = 40.sp,
+        textAlign = TextAlign.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(bottom = 20.dp)
+    )
 }
