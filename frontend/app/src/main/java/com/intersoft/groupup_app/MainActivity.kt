149503dbe9f97c71ab2ddc910c54c299148498cc
Nikola Pleić <npleic20@foi.hr>
2023-11-04 10:36:17 +0100

    frontend feat: Added basic navigation structure

--- frontend/app/src/main/java/com/intersoft/groupup_app/MainActivity.kt
+++ frontend/app/src/main/java/com/intersoft/groupup_app/MainActivity.kt
@@ -10,6 +10,11 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.intersoft.groupup_app.navigation.LoginPage
+import com.intersoft.groupup_app.navigation.RegistrationPage
 import com.intersoft.groupup_app.ui.theme.GroupUP_appTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,18 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    val navController = rememberNavController()
+
+                    NavHost(navController = navController, startDestination = "registration"){
+                        composable("registration"){
+                            RegistrationPage(
+                                onRegister = {navController.navigate("login")}
+                            )
+                        }
+                        composable("login"){
+                            LoginPage()
+                        }
+                    }
                 }
             }
         }
---
+++ frontend/app/src/main/java/com/intersoft/groupup_app/navigation/RegistrationPage.kt
@@ -0,0 +1,8 @@
+package com.intersoft.groupup_app.navigation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun RegistrationPage(onRegister: () -> Unit){
+
+}
