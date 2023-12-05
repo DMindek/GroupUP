cb1f1e8a84d9bdafee63b8d03907bd2361831b6c
DMindek <dmindek20@foi.hr>
2023-12-05 23:18:54 +0100

    frontend chore: added conversion of millis to Timestamp when sending data of new event to backend in EventRepository.kt

--- frontend/data/event/src/main/java/com/intersoft/event/EventRepository.kt
+++ frontend/data/event/src/main/java/com/intersoft/event/EventRepository.kt
@@ -1,10 +1,23 @@
 package com.intersoft.event
 
 import android.util.Log
+import com.intersoft.network.models.responses.EventData
 
 class EventRepository: IEventRepository {
 
     override fun createEvent(newEvent: EventModel, onCreateEventError: (String) -> Unit, onCreateEventSuccess: () -> Unit) {
         Log.d("EventRepository", newEvent.toString())
+        val dateTimestamp = java.sql.Timestamp(newEvent.dateInMillis)
+        Log.d("Event TimeStamp", dateTimestamp.toString())
+
+        val event = EventData(
+            name= newEvent.name,
+            description = newEvent.description,
+            date = dateTimestamp,
+            duration = 0,
+            maxParticipants = newEvent.maxParticipants,
+            location = newEvent.location,
+            ownerId =  newEvent.ownerId
+        )
     }
 }
