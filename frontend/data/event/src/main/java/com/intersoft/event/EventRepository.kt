81bf711d31c363178cc429f22cf14dab9486eb5f
DMindek <dmindek20@foi.hr>
2023-12-05 20:39:09 +0100

    frontend chore: added EventRepository.kt class

---
+++ frontend/data/event/src/main/java/com/intersoft/event/EventRepository.kt
@@ -0,0 +1,8 @@
+package com.intersoft.event
+
+class EventRepository: IEventRepository {
+
+    override fun createEvent(newEvent: EventModel, onCreateEventError: (String) -> Unit, onCreateEventSuccess: () -> Unit) {
+        //TODO
+    }
+}
