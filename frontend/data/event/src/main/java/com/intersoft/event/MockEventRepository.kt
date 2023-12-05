9e91c13ac359013e0353703f93700e3ebb998dca
DMindek <dmindek20@foi.hr>
2023-12-05 20:34:25 +0100

    frontend chore: added success callback to createEvent function in IEventRepository.kt

--- frontend/data/event/src/main/java/com/intersoft/event/MockEventRepository.kt
+++ frontend/data/event/src/main/java/com/intersoft/event/MockEventRepository.kt
@@ -8,9 +8,10 @@ class MockEventRepository : IEventRepository {
         EventModel("Test2","Test2", Instant.now().toEpochMilli() + 3600000,3600000,3,"location2"),
     )
 
-    override fun createEvent(newEvent: EventModel, onCreateEventError: (String) -> Unit) {
+    override fun createEvent(newEvent: EventModel, onCreateEventError: (String) -> Unit, onCreateEventSuccess: () -> Unit) {
         if(eventList.find { event -> (event.name == newEvent.name) && (event.location == newEvent.location) } == null){
             eventList.add(newEvent)
+            onCreateEventSuccess()
         }
         else{
             onCreateEventError("Event already exists")
