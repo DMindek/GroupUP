f5dc83ab6eef2ef04a74b72a38ce8e66896333a4
DMindek <dmindek20@foi.hr>
2023-12-06 12:33:06 +0100

    frontend chore: added conversion of date and star time of event to Timestamp in EventRepository.kt, also added conversion of durationInMillis to minutes

--- frontend/data/event/src/main/java/com/intersoft/event/EventRepository.kt
+++ frontend/data/event/src/main/java/com/intersoft/event/EventRepository.kt
@@ -7,9 +7,11 @@ class EventRepository: IEventRepository {
 
     override fun createEvent(newEvent: EventModel, onCreateEventError: (String) -> Unit, onCreateEventSuccess: () -> Unit) {
         Log.d("EventRepository", newEvent.toString())
-        val dateTimestamp = java.sql.Timestamp(newEvent.dateInMillis)
-        Log.d("Event TimeStamp", dateTimestamp.toString())
+        // Timestamp automatically adjusts to local time which is +1 offset for us, so we have to decrease the time by one hour in millis "3600000 ms" because it does not make sense to adjust starting time by 1 hour offset
+        val dateTimestamp = java.sql.Timestamp(newEvent.dateInMillis + newEvent.startTimeInMillis - 3600000 )
+        Log.d("EventRepository", dateTimestamp.toString())
         val durationInMinutes = (newEvent.durationInMillis / 60000).toInt()
+        Log.d("EventRepository", durationInMinutes.toString())
 
         val event = EventData(
             name= newEvent.name,
