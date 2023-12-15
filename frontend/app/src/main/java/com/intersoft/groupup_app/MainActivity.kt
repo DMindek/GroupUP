7eeea7b7e7bbe4a69f79e16874ee1e495cc7d40c
Nikola Pleić <npleic20@foi.hr>
2023-12-15 19:40:29 +0100

    frontend fix: fixed routing in MainActivity.kt

--- frontend/app/src/main/java/com/intersoft/groupup_app/MainActivity.kt
+++ frontend/app/src/main/java/com/intersoft/groupup_app/MainActivity.kt
@@ -36,7 +36,7 @@ class MainActivity : ComponentActivity() {
                 ) {
                     val navController = rememberNavController()
 
-                    NavHost(navController = navController, startDestination = "home"){
+                    NavHost(navController = navController, startDestination = "login"){
                         composable("registration"){
                             RegistrationPage(
                                 onRegister = {
@@ -48,11 +48,9 @@ class MainActivity : ComponentActivity() {
                         composable("login"){
                             LoginPage(
                                 context =LocalContext.current,
-                                onLogin = { navController.navigate("user_information") },
+                                onLogin = { navController.navigate("home") },
                                 onRegisterClick = { navController.navigate("registration") }
                             )
-                        }
-                        composable("main"){
                         }
                         composable("home"){
                             HomePage(
