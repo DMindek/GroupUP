7dbca9741839154a808ee4aeb2f702aeabb441eb
DMindek <dmindek20@foi.hr>
2023-12-19 14:02:34 +0100

    frontend feat: added getEvent function and StoredEventData data class  to EventManager.kt

--- frontend/feature/event/src/main/java/com/intersoft/event/EventManager.kt
+++ frontend/feature/event/src/main/java/com/intersoft/event/EventManager.kt
@@ -1,5 +1,7 @@
 package com.intersoft.event
 
+import java.sql.Timestamp
+
 object EventManager {
     private var eventRepository: IEventRepository = EventRepository()
 
@@ -31,6 +33,29 @@ object EventManager {
 
     }
 
+    fun getEvent(eventId: Int, onGetEventError: (String?) -> Unit, onGetEventSuccess: (StoredEventData) -> Unit){
+        eventRepository.getEvent(eventId,
+            onGetEventSuccess = {
+                val eventData = StoredEventData(
+                    id = it.id,
+                    name = it.name,
+                    description = it.description,
+                    date = it.date,
+                    duration = it.duration,
+                    max_participants = it.max_participants,
+                    location = it.location,
+                    owner_id = it.owner_id,
+                    participants = it.participants
+                )
+
+                onGetEventSuccess(eventData)
+            },
+            onGetEventError = {
+                onGetEventError(it)
+            }
+        )
+    }
+
     private fun validateInput(event: EventModel): String {
         if(event.name.isEmpty()) return "Please enter event name"
         if(event.name.length >= 20) return "Event name must contain less than 20 characters"
@@ -41,4 +66,17 @@ object EventManager {
 
         return ""
     }
+
+
+    data class StoredEventData (
+        val id : Int,
+        val name: String,
+        val description : String,
+        val date : Timestamp,
+        val duration : Int,
+        val max_participants : Int,
+        val location : String,
+        val owner_id : Int,
+        val participants : String?
+    )
 }
