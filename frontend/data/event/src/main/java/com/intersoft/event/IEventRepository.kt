43e598330f180684cbe6e89f7ce7b9988fb0e7ed
DMindek <dmindek20@foi.hr>
2023-12-19 13:44:43 +0100

    frontend chore: added getEvent function to IEventRepository.kt

--- frontend/data/event/src/main/java/com/intersoft/event/IEventRepository.kt
+++ frontend/data/event/src/main/java/com/intersoft/event/IEventRepository.kt
@@ -1,6 +1,8 @@
 package com.intersoft.event
 
+import com.intersoft.network.models.responses.StoredEventData
+
 interface IEventRepository {
-    fun createEvent(newEvent: EventModel, onCreateEventError: (String) -> Unit, onCreateEventSuccess: (String) -> Unit){
-    }
+    fun createEvent(newEvent: EventModel, onCreateEventError: (String) -> Unit, onCreateEventSuccess: (String) -> Unit){}
+    fun getEvent(eventId: Int, onGetEventSuccess: (StoredEventData) -> Unit, onGetEventError: (String?) -> Unit){}
 }
