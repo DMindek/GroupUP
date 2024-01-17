7809b611dba1820f0afaf1e85e041479c2659bab
DMindek <dmindek20@foi.hr>
2024-01-17 13:01:20 +0100

    frontend chore: added editEvent Function to IEventRepository.kt and EventRepository.kt

--- frontend/data/event/src/main/java/com/intersoft/event/EventRepository.kt
+++ frontend/data/event/src/main/java/com/intersoft/event/EventRepository.kt
@@ -49,6 +49,57 @@ class EventRepository: IEventRepository {
             }
         )
     }
+
+
+    override fun editEvent(
+        eventId: Int,
+        newEvent: EventModel,
+        authToken: String,
+        onCreateEventError: (String) -> Unit,
+        onCreateEventSuccess: (String) -> Unit
+    ) {
+        Log.d("EventRepository", newEvent.toString())
+        // Timestamp automatically adjusts to local time which is +1 offset for us, so we have to decrease the time by one hour in millis "3600000 ms" because it does not make sense to adjust starting time by 1 hour offset
+        val dateTimestamp = java.sql.Timestamp(newEvent.dateInMillis + newEvent.startTimeInMillis - 3600000 )
+        Log.d("EventRepository", dateTimestamp.toString())
+        val durationInMinutes = (newEvent.durationInMillis / 60000).toInt()
+        Log.d("EventRepository", durationInMinutes.toString())
+
+        val event = EventData(
+            name= newEvent.name,
+            description = newEvent.description,
+            date = dateTimestamp,
+            duration =durationInMinutes,
+            max_participants = newEvent.maxParticipants,
+            location = newEvent.location,
+            owner_id =  newEvent.ownerId
+        )
+
+        NetworkManager.editEvent(
+            eventId = eventId,
+            eventData = EventBody(event),
+            authToken = authToken,
+            onEditEventSuccess= {eventSuccessResponse ->
+                onCreateEventSuccess(eventSuccessResponse.toString())
+            },
+            onEditEventFail = {
+                Log.d("EventRepository", "Error occurred: $it")
+                if(!it.isNullOrEmpty())if(it[0] != '{') {
+                    onCreateEventError(it)
+                }
+                else{
+                    val error: CreateEventFailResponse
+                    try {
+                        error = Gson().fromJson(it, CreateEventFailResponse::class.java)
+                        Log.d("EventRepository", "Error occurred $error")
+                    }catch (e: Exception){
+                        onCreateEventError("Server returned unknown error")
+                        return@editEvent
+                    }
+                }
+            }
+        )
+    }
 }
 
 
--- frontend/data/event/src/main/java/com/intersoft/event/IEventRepository.kt
+++ frontend/data/event/src/main/java/com/intersoft/event/IEventRepository.kt
@@ -3,4 +3,7 @@ package com.intersoft.event
 interface IEventRepository {
     fun createEvent(newEvent: EventModel, onCreateEventError: (String) -> Unit, onCreateEventSuccess: (String) -> Unit){
     }
+
+    fun editEvent(eventId: Int,newEvent: EventModel,authToken: String ,onCreateEventError: (String) -> Unit, onCreateEventSuccess: (String) -> Unit){
+    }
 }
