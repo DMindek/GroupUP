4da0b3bc741108834827bda89eddf10e6628c152
DMindek <dmindek20@foi.hr>
2024-01-30 22:13:55 +0100

    frontend refactor: removed unused imports and formatting improved

--- frontend/feature/social/src/main/java/com/intersoft/social/UsersViewModel.kt
+++ frontend/feature/social/src/main/java/com/intersoft/social/UsersViewModel.kt
@@ -9,16 +9,6 @@ import com.intersoft.user.UserModel
 import com.intersoft.user.UserRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.combine
-import kotlinx.coroutines.flow.debounce
-import kotlinx.coroutines.flow.onEach
-import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
-import androidx.lifecycle.viewModelScope
-import kotlinx.coroutines.FlowPreview
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
 
 class UsersViewModel () : ViewModel() {
 
@@ -26,40 +16,19 @@ class UsersViewModel () : ViewModel() {
 
     private val _searchText = MutableStateFlow("")
 
-    val searchText = _searchText.asStateFlow()
-
     private val _isSearching = MutableStateFlow(false)
 
     val isSearching = _isSearching.asStateFlow()
 
-    //private val _authToken = MutableStateFlow("")
-
-    //val authToken = _authToken.asStateFlow()
-
-    //val _userList = getUsersByUsername(_searchText.value, _authToken.value)
-    //private var userList = listOf<UserModel>()
     var _users = MutableStateFlow(listOf<UserModel>())
 
     private val _error : MutableLiveData<String> = MutableLiveData("")
 
     val error: LiveData<String> = _error
 
-    /*
-    @OptIn(FlowPreview::class)
-    val users = _users
-        .debounce(1000L)
-        .onEach { _isSearching.update { true } }
-        .onEach { _isSearching.update { false } }
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(5000),
-            initialValue = _users
-        )
-*/
 
     fun onSearchTextChange(text: String, authToken: String) {
         _searchText.value = text
-        getUsersByUsername(text, authToken)
     }
 
 
